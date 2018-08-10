c0ab6e3c2f4fb56af598136fd2acced9585cdf7a
Dario Fadda <darioflute@gmail.com>
2018-08-10 11:54:06 -0700

    changed data directory in setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ config = {
     'author email': 'darioflute@gmail.com',
     'url': 'https://github.com/darioflute/elapsed.git',
     'download_url': 'https://github.com/darioflute/elapsed',
-    'license': 'MIT',
+    'license': 'GPL3',
     'packages': ['elapsed'],
     'scripts': ['bin/elapsed'],
     'include_package_data': True,
-    'package_data': {'sofia': ['icons/*.png', 'icons/*.gif', 'yellow.stylesheet', 'copyright.txt']}
+    'package_data': {'elapsed': ['icons/*.png', 'icons/*.gif', 'yellow.stylesheet', 'copyright.txt']}
 }
 
 setup(**config)
